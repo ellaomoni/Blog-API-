d8f1f794e291ff50e952aa1521c5962b93a698c4
Emmanuella Omoni <emmanuellaomoni184@gmail.com>
2025-01-28 21:45:06 +0100

    Reactored comment routes, Add comment routes to Post routes, Test all routes.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 const {createPost, getPosts, getPostbyId, updatePost, deletePost} = require('../controller/postController');
 const { verifyToken , isAdmin} = require('../middlewares/authMiddleware');
+const {addComment, deleteComment, getCommentsByPost} = require('../controller/commentController');
 
 
-
+//Post-related routes
 router.post('/create', verifyToken, createPost);
-router.get('/', getPosts);
+router.get('/posts', getPosts);
 router.get('/:id', getPostbyId);
 router.put('/:id', verifyToken, updatePost);
 router.delete('/:id',verifyToken, isAdmin, deletePost);
+
+
+//comment-related routes
+router.post('/:id/comments', verifyToken, addComment);
+router.get('/:id/comments', getCommentsByPost);
+router.delete('id:/comments/:commentId', verifyToken, deleteComment);
 
 module.exports = router;
